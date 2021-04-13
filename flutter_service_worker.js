67b89312117e72d504b1fce954604fcaa44b123d
nex-app-group <minhd_vu@yahoo.com>
2021-04-13 06:13:16 +0000

    Update from https://github.com/Nex-App-Group/scope-flutter/commit/e1c3be2a5fc5a522670d592f53694f08c489440f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,51 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "7f64366b71dfc23abc478b2abbd5a873",
-"index.html": "3027f023ba149495bbd602d060118a9c",
-"/": "3027f023ba149495bbd602d060118a9c",
-"main.dart.js": "4cc46072e410dff8bb97bca2283c2cd9",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "87740a2517522550cb237a902d6af258",
-".git/config": "4cc0cb147c974a0f78540b01ba902e64",
-".git/objects/34/58e06fce0bc96fdc75302ba1dd7bf1045d6f75": "9adb929fc03b5cd69a09845f2675dedb",
-".git/objects/20/5bb5db271c6d8de8399864c7bb9b917f638893": "c993b22f115d7f3ae6d5b7b212806539",
-".git/objects/73/9ef5e8c609ae4ea8f4911a2f69f36bb5b991ac": "4724a05cde9748d09e25a99b8bd3b056",
-".git/objects/8a/aa46ac1ae21512746f852a42ba87e4165dfdd1": "1d8820d345e38b30de033aa4b5a23e7b",
-".git/objects/4d/c38e1f503dc773d2cd45081856a0fc954e600c": "3f7ba9e0baf563c3d09f540a67717353",
-".git/objects/88/cfd48dff1169879ba46840804b412fe02fefd6": "e42aaae6a4cbfbc9f6326f1fa9e3380c",
-".git/objects/b7/49bfef07473333cf1dd31e9eed89862a5d52aa": "36b4020dca303986cad10924774fb5dc",
-".git/objects/cd/9596907bc3d0afb263caee61fbbc038266cf94": "923185cf1b9e61d67e635220c40abe1d",
-".git/HEAD": "4cf2d64e44205fe628ddd534e1151b58",
-".git/info/exclude": "036208b4a1ab4a235d75c181e685e5a3",
-".git/logs/HEAD": "193685cbc51dc01b4ca4253336489489",
-".git/logs/refs/heads/master": "193685cbc51dc01b4ca4253336489489",
-".git/logs/refs/remotes/origin/master": "43ff310eeca9ba323686dd8463e84746",
-".git/description": "a0a7c3fff21f2aea3cfa1d0316dd816c",
-".git/hooks/commit-msg.sample": "579a3c1e12a1e74a98169175fb913012",
-".git/hooks/pre-rebase.sample": "56e45f2bcbc8226d2b4200f7c46371bf",
-".git/hooks/pre-commit.sample": "305eadbbcd6f6d2567e033ad12aabbc4",
-".git/hooks/applypatch-msg.sample": "ce562e08d8098926a3862fc6e7905199",
-".git/hooks/fsmonitor-watchman.sample": "ea587b0fae70333bce92257152996e70",
-".git/hooks/pre-receive.sample": "2ad18ec82c20af7b5926ed9cea6aeedd",
-".git/hooks/prepare-commit-msg.sample": "2b5c047bdb474555e1787db32b2d2fc5",
-".git/hooks/post-update.sample": "2b7ea5cee3c49ff53d41e00785eb974c",
-".git/hooks/pre-merge-commit.sample": "39cb268e2a85d436b9eb6f47614c3cbc",
-".git/hooks/pre-applypatch.sample": "054f9ffb8bfe04a599751cc757226dda",
-".git/hooks/pre-push.sample": "2c642152299a94e05ea26eae11993b13",
-".git/hooks/update.sample": "647ae13c682f7827c22f5fc08a03674e",
-".git/hooks/push-to-checkout.sample": "c7ab00c7784efeadad3ae9b228d4b4db",
-".git/refs/heads/master": "ae25abbc71f788ae522d80735ebfa081",
-".git/refs/remotes/origin/master": "ae25abbc71f788ae522d80735ebfa081",
-".git/index": "25e6c2c910202c0804af47194e22b61c",
-".git/COMMIT_EDITMSG": "aa1066d729fce2bb8798336cc1007bae",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "84a8746409ef71029ed0c35dc33918b5",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"index.html": "9b3ffdf12662f9414ffec9102065502c",
+"/": "9b3ffdf12662f9414ffec9102065502c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac"
+"assets/NOTICES": "a49ff042686cfc1a82efbd99bfd704c5",
+"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"main.dart.js": "a51c95ac843413769c8c353d5991090a",
+"version.json": "83ed9fc4e63d3f02903b3b6a2fc5c7d6"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -65,7 +33,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
