787516f2ff9bfbb1debf948f40ddb176c80a9fbe
nex-app-group <minhd_vu@yahoo.com>
2021-04-26 17:45:13 +0000

    Update from https://github.com/Nex-App-Group/scope-flutter/commit/1cc0108efc6d4f7c3c3740ec22059181ed61cde1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,20 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+  "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"version.json": "83ed9fc4e63d3f02903b3b6a2fc5c7d6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "87740a2517522550cb237a902d6af258",
-"index.html": "c30661c447c14d632ca940e4d71c888c",
-"/": "c30661c447c14d632ca940e4d71c888c",
+"main.dart.js": "321e2111e48288d51685654521a4997f",
+"assets/NOTICES": "2feca01443f1e9c7f357b8f9e070ad9b",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/AssetManifest.json": "62d5b309766803222978f589168a5e54",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/NOTICES": "a49ff042686cfc1a82efbd99bfd704c5",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"main.dart.js": "4df6e287d2816d38e7a9b4926de2fc9b",
-"version.json": "83ed9fc4e63d3f02903b3b6a2fc5c7d6"
+"index.html": "3fb924b35b2427b611b41a7fa51f7011",
+"/": "3fb924b35b2427b611b41a7fa51f7011"
 };
 
 // The application shell files that are downloaded before a service worker can
