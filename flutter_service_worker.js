262e3c5ac1f3e3f2daa98e630fe00a14f182918b
nex-app-group <minhd_vu@yahoo.com>
2021-05-10 22:32:21 +0000

    Update from https://github.com/Nex-App-Group/scope-flutter/commit/7892da2643accf6194d1bbb5ef8701d31e488157

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,20 +3,20 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"version.json": "83ed9fc4e63d3f02903b3b6a2fc5c7d6",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "87740a2517522550cb237a902d6af258",
-"main.dart.js": "321e2111e48288d51685654521a4997f",
-"assets/NOTICES": "2feca01443f1e9c7f357b8f9e070ad9b",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/AssetManifest.json": "62d5b309766803222978f589168a5e54",
+  "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"index.html": "3fb924b35b2427b611b41a7fa51f7011",
-"/": "3fb924b35b2427b611b41a7fa51f7011"
+"assets/AssetManifest.json": "62d5b309766803222978f589168a5e54",
+"assets/NOTICES": "a7777a7c7b7687beb6dfe228ae908f6f",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"version.json": "83ed9fc4e63d3f02903b3b6a2fc5c7d6",
+"manifest.json": "87740a2517522550cb237a902d6af258",
+"main.dart.js": "0491a74c6519ea9950275812d6a958ef",
+"index.html": "9a34489c805c305702496e51857b67c2",
+"/": "9a34489c805c305702496e51857b67c2"
 };
 
 // The application shell files that are downloaded before a service worker can
